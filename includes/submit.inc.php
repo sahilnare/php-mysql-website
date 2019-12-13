0892fb52a9090b9196cfefdf8e6e89aa69dad50f
sahilnare <sahilnare78@gmail.com>
2019-12-13 13:53:30 +0530

    Added pattern regex

--- includes/submit.inc.php
+++ includes/submit.inc.php
@@ -17,12 +17,12 @@ if (isset($_POST['col-sub'])) {
 		exit();
 	}
 
-	else if (!preg_match("/^[a-zA-Z]*$/", $name_college)) {
+	else if (!preg_match("/^\w+( \w+)*$/", $name_college)) {
 		header("Location: ../submit.php?error=invalidname&insname=".$name_college);
 		exit();
 	}
 
-	else if (!preg_match("/^[a-zA-Z]*$/", $location)) {
+	else if (!preg_match("/^\w+( \w+)*$/", $location)) {
 		header("Location: ../submit.php?error=invalidlocation&location=".$location);
 		exit();
 	}
@@ -44,7 +44,7 @@ if (isset($_POST['col-sub'])) {
 				exit();
 			}
 			else {
-				$sql = "INSERT INTO colleges (nameCollege, location) VALUES (?, ?, ?)";
+				$sql = "INSERT INTO colleges (nameCollege, location) VALUES (?, ?)";
 				$stmt = mysqli_stmt_init($conn);
 				if (!mysqli_stmt_prepare($stmt, $sql)) {
 					header("Location: ../submit.php?error=sqlerror");
@@ -53,10 +53,19 @@ if (isset($_POST['col-sub'])) {
 				else {
 					mysqli_stmt_bind_param($stmt, "ss", $name_college, $location);
 					mysqli_stmt_execute($stmt);
-					mysqli_stmt_store_result($stmt);
+					header("Location: ../submit.php?submition=success");
+					exit();
 				}
 			}
 		}
 	}
 
+	mysqli_stmt_close($stmt);
+	mysqli_close($conn);
+
+}
+
+else {
+	header("Location: ../submit.php");
+	exit();
 }
