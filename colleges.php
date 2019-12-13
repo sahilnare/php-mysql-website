c41ae200f7313926eacb7a32728f5e1180ab0c07
sahilnare <sahilnare78@gmail.com>
2019-12-13 15:26:51 +0530

    Added tables

--- colleges.php
+++ colleges.php
@@ -5,14 +5,36 @@
 
 <main>
 	<div class="container is-fluid">
-	<form action="includes/getcollege.inc.php" method="get">
+	<!-- <form action="includes/getcollege.inc.php" method="get">
 		
 		<div class="field is-grouped">
 		  <div class="control">
 		    <button class="button is-link" name="col-get" type="submit">Get Colleges</button>
 		  </div>
 		</div>
-	</form>
+	</form> -->
+	<?php
+		require 'includes/dbh.inc.php';
+
+		$sql = "SELECT nameCollege,location FROM colleges";
+		$result = mysqli_query($conn, $sql);
+		$resultCheck = mysqli_num_rows($result);
+		$college_name = array();
+		$college_location = array();
+
+		if ($resultCheck > 0) {
+			while ($row = mysqli_fetch_assoc($result)) {
+				array_push($college_name, $row['nameCollege']);
+				array_push($college_location, $row['location']);
+			}
+			print_r($college_name);
+			print_r($college_location);
+		}
+		else {
+			echo "No colleges";
+		}
+	?>
+
 	</div>
 </main>
 
--- includes/getcollege.inc.php
+++ includes/getcollege.inc.php
@@ -4,8 +4,21 @@ if (isset($_GET['col-get'])) {
 	require 'dbh.inc.php';
 
 	$sql = "SELECT nameCollege,location FROM colleges";
+	$result = mysqli_query($conn, $sql);
+	$resultCheck = mysqli_num_rows($result);
+
+	if ($resultCheck > 0) {
+		while ($row = mysqli_fetch_assoc($result)) {
+			echo $row['nameCollege'];
+		}
+	}
+	else {
+		echo "No colleges";
+	}
 }
 else {
 	header("Location: ../index.php");
 	exit();
 }
+
+?>
